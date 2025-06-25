5b09df9ba450ce576683f72e87c87f0a08d89021
Leighton Pulliam II <167991973+L8onii@users.noreply.github.com>
2025-06-25 12:12:04 -0400

    Quote Card 3

--- public/js/index.js
+++ public/js/index.js
@@ -1,38 +1,56 @@
-"use strict"
+async function getRandomImage() {
+    const client_id = "rMnB2bJVSMZX6i0XdIlyz_-lMShFAlKysb28xv5bljA";
+    const endpoint = `https://api.unsplash.com/photos/random/?client_id=${client_id}`;
+    try {
+        const response = await fetch(endpoint);
+        const returnedData = await response.json();
+        const receivedPhotoUrl = returnedData.urls.regular;
 
-const elements = {
-    quote: document.getElementById("quote"),
-    author: document.getElementById("author"),
-};
-
-const quotes = [
-    {
-        quote: "All hands! Abandon ship!",
-        author: "Captain Picard",
-    },
-
-    {
-        quote: "Doh!",
-        author: "Homer Simpson",
-    },
-
-    {
-        quote: "Many of life’s failures are people who did not realize how close they were to success when they gave up.",
-        author: "Thomas A. Edison",
+        const imgDiv = document.querySelector(".background-img");
+        imgDiv.style.backgroundImage = `url("${receivedPhotoUrl}")`;
+    } catch (error) {
+        console.error(error);
     }
-]
+}    
+getRandomImage();
 
-function loopThroughQuotes() {
-    let quoteIndex = 0;
-    setInterval(() => {
-        if (quoteIndex < quotes.length) {
-            elements.quote.textContent = quotes[quoteIndex].quote;
-            elements.author.textContent = quotes[quoteIndex].author;
-            quoteIndex++;
-        } else {
-            quoteIndex = 0;
-        }
-    }, 3000);
-}
+// Practice Stuff - See The Quote Card Express: Part 3
 
-setTimeout(loopThroughQuotes, 3000);
+// "use strict"
+
+// const elements = {
+//     quote: document.getElementById("quote"),
+//     author: document.getElementById("author"),
+// };
+
+// const quotes = [
+//     {
+//         quote: "All hands! Abandon ship!",
+//         author: "Captain Picard",
+//     },
+
+//     {
+//         quote: "Doh!",
+//         author: "Homer Simpson",
+//     },
+
+//     {
+//         quote: "Many of life’s failures are people who did not realize how close they were to success when they gave up.",
+//         author: "Thomas A. Edison",
+//     }
+// ]
+
+// function loopThroughQuotes() {
+//     let quoteIndex = 0;
+//     setInterval(() => {
+//         if (quoteIndex < quotes.length) {
+//             elements.quote.textContent = quotes[quoteIndex].quote;
+//             elements.author.textContent = quotes[quoteIndex].author;
+//             quoteIndex++;
+//         } else {
+//             quoteIndex = 0;
+//         }
+//     }, 3000);
+// }
+
+// setTimeout(loopThroughQuotes, 3000);
