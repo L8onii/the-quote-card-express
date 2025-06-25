b0e2701882aeae1b6993125d8cb2317493534d67
Leighton Pulliam II <167991973+L8onii@users.noreply.github.com>
2025-06-25 11:22:00 -0400

    new feature - quote loop

---
+++ public/js/index.js
@@ -0,0 +1,38 @@
+"use strict"
+
+const elements = {
+    quote: document.getElementById("quote"),
+    author: document.getElementById("author"),
+};
+
+const quotes = [
+    {
+        quote: "All hands! Abandon ship!",
+        author: "Captain Picard",
+    },
+
+    {
+        quote: "Doh!",
+        author: "Homer Simpson",
+    },
+
+    {
+        quote: "Many of life’s failures are people who did not realize how close they were to success when they gave up.",
+        author: "Thomas A. Edison",
+    }
+]
+
+function loopThroughQuotes() {
+    let quoteIndex = 0;
+    setInterval(() => {
+        if (quoteIndex < quotes.length) {
+            elements.quote.textContent = quotes[quoteIndex].quote;
+            elements.author.textContent = quotes[quoteIndex].author;
+            quoteIndex++;
+        } else {
+            quoteIndex = 0;
+        }
+    }, 3000);
+}
+
+setTimeout(loopThroughQuotes, 3000);
